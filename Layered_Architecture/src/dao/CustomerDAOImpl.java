eb77289268a3feef2d2465595bfabc8c304b62ee
lakshanheratg <lakshanherath1101@gmail.com>
2022-05-26 23:16:46 -0700

    created uniq interfaces

---
+++ Layered_Architecture/src/dao/CustomerDAOImpl.java
@@ -0,0 +1,64 @@
+package dao;
+
+import model.CustomerDTO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class CustomerDAOImpl implements CustomerDAO {
+    @Override
+    public ArrayList<CustomerDTO> getAll() throws SQLException, ClassNotFoundException {
+        ResultSet rst = SQLUtil.executeQuery("SELECT * FROM Customer");
+        ArrayList<CustomerDTO> allCustomers = new ArrayList<>();
+        while (rst.next()) {
+            allCustomers.add(new CustomerDTO(rst.getString(1), rst.getString(2), rst.getString(3)));
+        }
+        return allCustomers;
+    }
+
+    @Override
+    public boolean save(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        return SQLUtil.executeUpdate("INSERT INTO Customer (id,name, address) VALUES (?,?,?)", dto.getId(), dto.getName(), dto.getAddress());
+    }
+
+    @Override
+    public boolean update(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+        return SQLUtil.executeUpdate("UPDATE Customer SET name=?, address=? WHERE id=?", dto.getName(), dto.getAddress(), dto.getId());
+    }
+
+    @Override
+    public CustomerDTO search(String s) throws SQLException, ClassNotFoundException {
+        ResultSet rst = SQLUtil.executeQuery("SELECT * FROM Customer WHERE id=?", s);
+        if (rst.next()) {
+            return new CustomerDTO(rst.getString(1), rst.getString(2), rst.getString(3));
+        }else {
+            return null;
+        }
+
+    }
+
+    @Override
+    public boolean exist(String s) throws SQLException, ClassNotFoundException {
+        return SQLUtil.executeQuery("SELECT id FROM Customer WHERE id=?", s).next();
+    }
+
+    @Override
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return SQLUtil.executeUpdate("DELETE FROM Customer WHERE id=?", s);
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        ResultSet rst = SQLUtil.executeQuery("SELECT id FROM Customer ORDER BY id DESC LIMIT 1;");
+        if (rst.next()) {
+            String id = rst.getString("id");
+            int newCustomerId = Integer.parseInt(id.replace("C00-", "")) + 1;
+            return String.format("C00-%03d", newCustomerId);
+        } else {
+            return "C00-001";
+        }
+    }
+
+}
---
+++ Layered_Architecture/src/dao/OrderDao.java
@@ -0,0 +1,6 @@
+package dao;
+
+import model.OrderDTO;
+
+public interface OrderDao extends CrudDAO<OrderDTO, String> {
+}
